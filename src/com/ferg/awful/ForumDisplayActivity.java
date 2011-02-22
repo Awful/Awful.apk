02292485943432a53bbfad0e5930090ef054959d
James Power <heathen79@gmail.com>
2011-02-21 23:49:12 -0500

    Added endless scrolling to Forum ListView, when a user scrolls down the next page of threads are loaded and added to the array of threads automatically.

--- src/com/ferg/awful/ForumDisplayActivity.java
+++ src/com/ferg/awful/ForumDisplayActivity.java
@@ -44,11 +44,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AbsListView.OnScrollListener;
 
 import org.htmlcleaner.TagNode;
 
@@ -62,9 +65,11 @@ public class ForumDisplayActivity extends Activity {
 	private AwfulForum mForum;
 
     private ListView mThreadList;
-	private ProgressDialog mDialog;
+    private ArrayList<AwfulThread> mThreads = new ArrayList<AwfulThread>();
+    private AwfulThreadAdapter mThreadAdapter;
+    private ProgressDialog mDialog;
     private SharedPreferences mPrefs;
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -72,9 +77,13 @@ public class ForumDisplayActivity extends Activity {
         setContentView(R.layout.forum_display);
 		
         mPrefs = getSharedPreferences(Constants.PREFERENCES, MODE_PRIVATE);
-
+        mThreadAdapter = new AwfulThreadAdapter(ForumDisplayActivity.this, 
+                R.layout.thread_item, mThreads);
+        
         mThreadList = (ListView) findViewById(R.id.forum_list);
-
+        mThreadList.setOnScrollListener(new EndlessScrollListener());
+        mThreadList.setAdapter(mThreadAdapter);
+        
 		mForum = (AwfulForum) getIntent().getParcelableExtra(Constants.FORUM);
 
         new FetchThreadsTask().execute(mForum.getForumId());
@@ -155,11 +164,10 @@ public class ForumDisplayActivity extends Activity {
         }
 
         public void onPostExecute(ArrayList<AwfulThread> aResult) {
-            mThreadList.setAdapter(new AwfulThreadAdapter(ForumDisplayActivity.this, 
-                        R.layout.thread_item, aResult));
-
+        	mThreads.addAll(aResult);
+        	mThreadAdapter.notifyDataSetChanged();
             mThreadList.setOnItemClickListener(onThreadSelected);
-
+            
             mDialog.dismiss();
         }
     }
@@ -216,5 +224,36 @@ public class ForumDisplayActivity extends Activity {
 
             return inflatedView;
         }
+    }
+    
+    private class EndlessScrollListener implements OnScrollListener {
+    	private int visibleThreshold = 5;
+    	private int currentPage = 0;
+    	private int previousTotal = 0;
+    	private boolean loading = true;
+    	
+    	public EndlessScrollListener() {
+    	}
+    	
+    	public void onScroll(AbsListView view, int firstVisibleItem,
+    			int visibleItemCount, int totalItemCount) {
+    		if (loading) {
+    			if (totalItemCount > previousTotal) {
+    				loading = false;
+    				previousTotal = totalItemCount;
+    				currentPage++;
+    			}
+    		}
+    		
+    		if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+    			//load new items in background and add them
+    			loading = true;
+    			new FetchThreadsTask(mForum.getCurrentPage() + 1).execute(mForum.getForumId());
+    		}
+    	}
+
+    	public void onScrollStateChanged(AbsListView view, int scrollState) {
+    		
+    	}
     }
 }
