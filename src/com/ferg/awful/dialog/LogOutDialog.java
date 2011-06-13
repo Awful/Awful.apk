3a27b8b73e375ad22bd81e29a96b0b1c9256bb50
Nelson Chen <crazysim@gmail.com>
2011-06-13 00:39:30 -0700

    logout dialog skeleton

---
+++ src/com/ferg/awful/dialog/LogOutDialog.java
@@ -0,0 +1,10 @@
+package com.ferg.awful.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class LogOutDialog extends Dialog {
+    public LogOutDialog(Context context) {
+        super(context);
+    }
+}
