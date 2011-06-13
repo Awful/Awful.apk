cab2c99310e955eb0258bcb77ff9e0269398dda1
Nelson Chen <crazysim@gmail.com>
2011-06-13 01:14:34 -0700

    extends alertdialog

--- src/com/ferg/awful/dialog/LogOutDialog.java
+++ src/com/ferg/awful/dialog/LogOutDialog.java
@@ -1,9 +1,9 @@
 package com.ferg.awful.dialog;
 
-import android.app.Dialog;
+import android.app.AlertDialog;
 import android.content.Context;
 
-public class LogOutDialog extends Dialog {
+public class LogOutDialog extends AlertDialog {
     public LogOutDialog(Context context) {
         super(context);
     }
