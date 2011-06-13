5d3a9a118149af5c9b232bef0a03e2ca5665c862
Nelson Chen <crazysim@gmail.com>
2011-06-13 01:47:07 -0700

    added buttons for dialog

--- src/com/ferg/awful/dialog/LogOutDialog.java
+++ src/com/ferg/awful/dialog/LogOutDialog.java
@@ -2,6 +2,7 @@ package com.ferg.awful.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import com.ferg.awful.R;
 
 public class LogOutDialog extends AlertDialog {
@@ -9,5 +10,7 @@ public class LogOutDialog extends AlertDialog {
         super(context);
         setTitle(context.getString(R.string.logout));
         setMessage(context.getString(R.string.logout_message));
+        setButton(context.getString(R.string.logout), (OnClickListener) null);
+        setButton2(context.getString(R.string.cancel), (OnClickListener) null);
     }
 }
