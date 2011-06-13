7439b3616e6a3672347f0eff865767c15ab4254d
Nelson Chen <crazysim@gmail.com>
2011-06-13 01:39:09 -0700

    dialog box uses strings.xml

--- src/com/ferg/awful/dialog/LogOutDialog.java
+++ src/com/ferg/awful/dialog/LogOutDialog.java
@@ -2,11 +2,12 @@ package com.ferg.awful.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import com.ferg.awful.R;
 
 public class LogOutDialog extends AlertDialog {
     public LogOutDialog(Context context) {
         super(context);
-        setMessage("Hello.");
-        setTitle("Hi!");
+        setTitle(context.getString(R.string.logout));
+        setMessage(context.getString(R.string.logout_message));
     }
 }
