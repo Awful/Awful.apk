0a467f4af07875959bf64493bd5b268c9bfa0972
Sereri <christoph.r@gmx.net>
2011-06-30 01:31:17 +0200

    There can be only one (UserCPFragment). Color constants are gone. Invalid reply dialog a little bit cleaner.

--- src/com/ferg/awful/UserCPFragment.java
+++ src/com/ferg/awful/UserCPFragment.java
@@ -156,7 +156,6 @@ public class UserCPFragment extends DialogFragment implements AwfulUpdateCallbac
     @Override
     public void onDetach() {
         super.onDetach();
-        if(mBookmarkList != null)
         mBookmarkList.setAdapter(null);
     }
     
