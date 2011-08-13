ef0bfdcb57ec6f6daffb73c25ed7053ef02d5e07
Geekner <mattshepard@gmail.com>
2011-08-13 16:35:02 -0700

    Fixed crash-on-reload in UserCP.

--- src/com/ferg/awful/UserCPFragment.java
+++ src/com/ferg/awful/UserCPFragment.java
@@ -262,7 +262,7 @@ public class UserCPFragment extends DialogFragment implements AwfulUpdateCallbac
 
     @Override
     public void dataUpdate(boolean pageChange) {
-        if(pageChange && this.isAdded() && mBookmarkList!= null && mBookmarkList.getChildCount() >0){
+        if(pageChange && this.isAdded() && mBookmarkList!= null && mBookmarkList.getCount() >0){
             mBookmarkList.setSelection(0);
         }
     }
