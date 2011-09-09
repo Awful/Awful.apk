8e05e94544e28843f6cd76d9744e924cfe136464
Scott Ferguson <scottwferg@gmail.com>
2011-09-09 15:15:01 -0500

    Stable page indicator

--- assets/thread.js
+++ assets/thread.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    var clear = function() {
+        $("#thread-body").html("");
+    };
+
     $('.action-button').live('click', function(event) {
         if ($(this).hasClass("editable")) {
             listener.onEditablePostClick($(this).attr('id'), $(this).attr('lastreadurl'));
@@ -7,16 +11,9 @@ $(document).ready(function() {
         }
     });
 
-    $('#previous-page').live('click', function(event) {
-        listener.onPreviousPageClick();
-    });
-
-    $('#next-page').live('click', function(event) {
-        listener.onNextPageClick();
-    });
-
     var posts = JSON.parse(post_list);
     var prefs = JSON.parse(preferences);
+    var page = JSON.parse(pager);
 
     var thread = $("#thread-body");
     var light = true;
@@ -54,9 +51,27 @@ $(document).ready(function() {
     }
 
     $('.content').append(ich.pagefooter({
-        currentPage: currentPage,
-        pageTotal: pageTotal,
+        currentPage: page.currentPage,
+        pageTotal: page.pageTotal,
+        nextPageButton: (page.isLastPage == "true") ? "stat_notify_sync.png" : "r_arrow.png",
     }));
+
+    $('#previous-page').bind('click', function(event) {
+        clear();
+        listener.onPreviousPageClick();
+    });
+
+    if (page.isLastPage == "false") {
+        $('#next-page').bind('click', function(event) {
+            clear();
+            listener.onNextPageClick();
+        });
+    } else {
+        $('#next-page').bind('click', function(event) {
+            clear();
+            listener.onRefreshPageClick();
+        });
+    }
 
     var salr = new SALR(prefs);
 
