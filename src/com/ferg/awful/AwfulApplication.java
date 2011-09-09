8c50cd9098d0b87abd215ded8954f0e4c1e2ea55
Scott Ferguson <scottwferg@gmail.com>
2011-09-09 09:38:52 -0500

    Add ACRA and Bugsense tracking

--- src/com/ferg/awful/AwfulApplication.java
+++ src/com/ferg/awful/AwfulApplication.java
@@ -2,12 +2,23 @@ package com.ferg.awful;
 
 import android.app.Application;
 
+import org.acra.*;
+import org.acra.annotation.*;
+
+import com.ferg.awful.constants.Constants;
+
 /**
  * Allows application-wide access to the global image cache
  *
  * @author brosmike
  */
+@ReportsCrashes(formUri = "http://www.bugsense.com/api/acra?api_key=d6a53a0d", formKey = Constants.ACRA_FORMKEY) 
 public class AwfulApplication extends Application {
 	private static String TAG="AwfulApplication";
 
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+    }
 }
