1abb16546fce90cedbe6ca5bc2bb1629b8ce2d6d
Geekner <mattshepard@gmail.com>
2011-11-05 10:42:09 -0700

    Fixed CPU leak on threadview.

--- assets/thread.js
+++ assets/thread.js
@@ -11,23 +11,31 @@ $(document).ready(function() {
 
     var salr = new SALR(prefs);
 
-    setYPosition();
+    scrollPost();
 });
 
 $(window).load(function() {
-    setYPosition();
+    scrollPost();
 });
 
-function setYPosition() {
-	if(prefs.yPos == "-1"){
-	    if (prefs.postjumpid != "") {
-	    	try{
-	    		$(window).scrollTop($("#".concat(prefs.postjumpid)).first().offset().top);
-	    	}catch(error){}
-	    } else {
-		    try{
-		        $(window).scrollTop($('.unread').first().offset().top);
-		    }catch(error){}
-	    }
+function scrollPost() {
+    if (prefs.postjumpid != "") {
+    	try{
+    		$(window).scrollTop($("#".concat(prefs.postjumpid)).first().offset().top);
+    	}catch(error){
+    		scrollLastRead();
+    	}
+    } else {
+	    scrollLastRead();
+    }
+}
+
+function scrollLastRead(){
+	try{
+        $(window).scrollTop($('.unread').first().offset().top);
+    }catch(error){
+    	try{
+    		$(window).scrollTop($('.read').last().offset().top);
+		}catch(error){}
     }
 }
