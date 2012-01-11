41eee8c0ccc22aa4a5ffe31efb09a16a84e099e1
Matt Shepard <mattshepard@gmail.com>
2012-01-11 14:27:56 -0700

    Forgot to add this.

---
+++ src/com/ferg/awful/thread/AwfulEmote.java
@@ -0,0 +1,38 @@
+package com.ferg.awful.thread;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ferg.awful.preferences.AwfulPreferences;
+
+public class AwfulEmote implements AwfulDisplayItem {
+
+	public static final String ID = "_id";
+	public static final String TEXT = "text";
+	public static final String SUBTEXT = "subtext";//hover text
+	public static final String URL = "url";
+	public static final String CACHEFILE = "cachefile";//location of cached file or null if not cached yet.
+	@Override
+	public View getView(LayoutInflater inf, View current, ViewGroup parent,
+			AwfulPreferences aPref) {
+		return null;
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DISPLAY_TYPE getType() {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
