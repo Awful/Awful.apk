76a137d01caf9767b40b773405a1992fb9d03dd4
Scott Ferguson <scottwferg@gmail.com>
2012-01-18 21:45:01 -0600

    Change directory structure to allow for library projects

---
+++ application/bin/classes.dex.d
@@ -0,0 +1,11 @@
+/home/scott/Dropbox/Projects/awful_android/application/bin/classes.dex : \
+/home/scott/Dropbox/Projects/awful_android/application/libs/acra-4.2.3.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/android-support-v4.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/bugsense-trace.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/commons-lang3-3.0.1.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/htmlcleaner-2.2.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/httpclient-4.1.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/httpcore-4.1.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/httpmime-4.1.jar \
+/home/scott/Dropbox/Projects/awful_android/application/libs/libGoogleAnalytics.jar \
+/home/scott/Dropbox/Projects/awful_android/libraries/LeftNavBarLibrary/bin/classes.jar \
