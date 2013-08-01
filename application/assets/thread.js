b4400c703cda1130aae04aef4e3cee323e3a8179
Sereri <christoph.r@gmx.net>
2013-08-01 13:20:50 +0200

    FYAD fix, css fixes, version code 67

--- application/assets/thread.js
+++ application/assets/thread.js
@@ -1,16 +1,16 @@
 var prefs = JSON.parse(preferences);
 
 function toggleinfo(info){
-	if($(info).children('.postinfo-regdate').hasClass('extended')){
+	if($(info).children('.postinfo-title').hasClass('extended')){
 		$(info).children('.avatar-cell').removeClass('extended');
 		$(info).children('.avatar-cell').children('.avatar').removeClass('extended');
-		$(info).children('.postinfo-regdate').removeClass('extended');
 		$(info).children('.postinfo-title').removeClass('extended');
+		$(info).children('.postinfo-regdate').removeClass('extended');
 	}else{
 		$(info).children('.avatar-cell').addClass('extended');
 		$(info).children('.avatar-cell').children('.avatar').addClass('extended');
-		$(info).children('.postinfo-regdate').addClass('extended');
 		$(info).children('.postinfo-title').addClass('extended');
+		$(info).children('.postinfo-regdate').addClass('extended');
 	}
 }
 function toggleoptions(menu){
