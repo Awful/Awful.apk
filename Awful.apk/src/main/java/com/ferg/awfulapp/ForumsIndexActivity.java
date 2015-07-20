c74274529022147b7867daa07b058f0035469dda
sereri <christoph.r@gmx.net>
2015-07-20 16:37:10 +0200

    Removed Icons hack

--- Awful.apk/src/main/java/com/ferg/awfulapp/ForumsIndexActivity.java
+++ Awful.apk/src/main/java/com/ferg/awfulapp/ForumsIndexActivity.java
@@ -773,28 +773,6 @@ public class ForumsIndexActivity extends AwfulActivity {
 
 
     @Override
-    public boolean onMenuOpened(int featureId, Menu menu)
-    {
-        if(featureId == Window.FEATURE_ACTION_BAR && menu != null){
-            if(menu.getClass().getSimpleName().equals("MenuBuilder")){
-                try{
-                    Method m = menu.getClass().getDeclaredMethod(
-                            "setOptionalIconsVisible", Boolean.TYPE);
-                    m.setAccessible(true);
-                    m.invoke(menu, true);
-                }
-                catch(NoSuchMethodException e){
-                    Log.e(TAG, "onMenuOpened", e);
-                }
-                catch(Exception e){
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        return super.onMenuOpened(featureId, menu);
-    }
-
-    @Override
     public void displayThread(int id, int page, int forumId, int forumPg, boolean forceReload) {
         Log.d(TAG, "displayThread " + id + " " + forumId);
         if (mViewPager != null) {
