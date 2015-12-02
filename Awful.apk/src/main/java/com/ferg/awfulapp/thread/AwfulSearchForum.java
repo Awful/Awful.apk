0ff979afe725810bb3979fdcfbffa174c01cb2d2
sereri <christoph.r@gmx.net>
2015-12-02 18:37:48 +0100

    Search: checking  forums also checks sub-forums

--- Awful.apk/src/main/java/com/ferg/awfulapp/thread/AwfulSearchForum.java
+++ Awful.apk/src/main/java/com/ferg/awfulapp/thread/AwfulSearchForum.java
@@ -5,6 +5,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Christoph on 30.11.2015.
@@ -14,6 +16,8 @@ public class AwfulSearchForum {
     private int mForumId;
     private String mForumName;
     private boolean mChecked;
+    private int mDepth;
+    private Set<String> mParents = new HashSet<>();
 
     public boolean isChecked() {
         return mChecked;
@@ -39,6 +43,22 @@ public class AwfulSearchForum {
         this.mForumName = aForumName;
     }
 
+    public Set<String> getParents() {
+        return mParents;
+    }
+
+    public void setParents(Set<String> aParents) {
+        this.mParents = aParents;
+    }
+
+    public int getDepth() {
+        return mDepth;
+    }
+
+    public void setDepth(int aDepth) {
+        this.mDepth = aDepth;
+    }
+
     public static ArrayList<AwfulSearchForum> parseSearchForums(Document doc) {
         ArrayList<AwfulSearchForum> searchForums = new ArrayList<>();
 
@@ -51,7 +71,26 @@ public class AwfulSearchForum {
                     AwfulSearchForum searchForum =  new AwfulSearchForum();
                     searchForum.setChecked(forum.hasClass("checked"));
                     searchForum.setForumId(Integer.parseInt(forum.attr("data-forumid")));
-                    searchForum.setForumName(forum.text());
+                    String forumName = forum.text();
+                    if(forum.hasClass("depth1")){
+                        forumName = " "+ forumName;
+                        searchForum.setDepth(1);
+                    }else if(forum.hasClass("depth2")){
+                        forumName = "  "+ forumName;
+                        searchForum.setDepth(2);
+                    }else if(forum.hasClass("depth3")){
+                        forumName = "   "+ forumName;
+                        searchForum.setDepth(3);
+                    }else{
+                        searchForum.setDepth(0);
+                    }
+                    searchForum.setForumName(forumName);
+
+                    for (String className : forum.classNames()){
+                        if(className.startsWith("parent")){
+                            searchForum.getParents().add(className);
+                        }
+                    }
                     searchForums.add(searchForum);
                 }
             }
