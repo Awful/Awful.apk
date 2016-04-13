b1eb069bc0457a2f2f9f58407b7c66ae28c78ab0
sereri <christoph.r@gmx.net>
2016-04-13 19:16:45 +0200

    Preview seems to work pretty good after putting the webview into a nested scrollview. I'm sure this won't come back to haunt us.

--- Awful.apk/src/main/java/com/ferg/awfulapp/PreviewFragment.java
+++ Awful.apk/src/main/java/com/ferg/awfulapp/PreviewFragment.java
@@ -1,18 +1,18 @@
 /********************************************************************************
  * Copyright (c) 2012, Matthew Shepard
  * All rights reserved.
- * 
+ * <p/>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the software nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the software nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * <p/>
  * THIS SOFTWARE IS PROVIDED BY SCOTT FERGUSON ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,108 +27,150 @@
 
 package com.ferg.awfulapp;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.ImageView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import com.android.volley.VolleyError;
 import com.ferg.awfulapp.constants.Constants;
-import com.ferg.awfulapp.network.NetworkUtils;
 import com.ferg.awfulapp.preferences.AwfulPreferences;
 import com.ferg.awfulapp.provider.ColorProvider;
-import com.ferg.awfulapp.task.AwfulRequest;
-import com.ferg.awfulapp.task.PreviewPostRequest;
-import com.ferg.awfulapp.task.SearchForumsRequest;
-import com.ferg.awfulapp.thread.AwfulAction;
 import com.ferg.awfulapp.thread.AwfulMessage;
-import com.ferg.awfulapp.thread.AwfulSearchForum;
 import com.ferg.awfulapp.util.AwfulUtils;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PreviewFragment extends AwfulDialogFragment {
-	private final static String TAG = "PreviewFragment";
+    private final static String TAG = "PreviewFragment";
 
-	private WebView postPreView;
-	private ProgressBar previewProgress;
+    private WebView postPreView;
+    private ProgressBar previewProgress;
+    private View dialogView;
 
+    HashMap<String, String> preferences;
 
     @Override
-	public void onActivityCreated(Bundle aSavedState) {
-		super.onActivityCreated(aSavedState);
-	}
+    public void onActivityCreated(Bundle aSavedState) {
+        super.onActivityCreated(aSavedState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View result = inflateView(R.layout.post_preview, container, inflater);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dialogView = inflateView(R.layout.post_preview, container, inflater);
 
-		postPreView = (WebView) result.findViewById(R.id.post_pre_view);
-		postPreView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-		postPreView.getSettings().setJavaScriptEnabled(true);
-		postPreView.getSettings().setRenderPriority(WebSettings.RenderPriority.LOW);
-		postPreView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
-		postPreView.getSettings().setDefaultFontSize(mPrefs.postFontSizeDip);
-		postPreView.getSettings().setDefaultFixedFontSize(mPrefs.postFixedFontSizeDip);
-		if(Constants.DEBUG && AwfulUtils.isKitKat()) {
-			WebView.setWebContentsDebuggingEnabled(true);
-		}
-		if(AwfulUtils.isLollipop()){
-			postPreView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-		}
-		if (mPrefs.inlineYoutube || mPrefs.inlineWebm || mPrefs.inlineVines) {//YOUTUBE SUPPORT BLOWS
-			postPreView.getSettings().setPluginState(WebSettings.PluginState.ON_DEMAND);
-		}
-		if ( AwfulUtils.isAtLeast(Build.VERSION_CODES.JELLY_BEAN_MR1) && (mPrefs.inlineWebm || mPrefs.inlineVines)) {
-			postPreView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-		}
-		if (mPrefs.inlineTweets && AwfulUtils.isJellybean()) {
-			postPreView.getSettings().setAllowUniversalAccessFromFileURLs(true);
-			postPreView.getSettings().setAllowFileAccessFromFileURLs(true);
-			postPreView.getSettings().setAllowFileAccess(true);
-			postPreView.getSettings().setAllowContentAccess(true);
-		}
+        configureWebView();
 
-		if (!mPrefs.enableHardwareAcceleration) {
-			postPreView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
-		}
-		previewProgress = (ProgressBar) result.findViewById(R.id.preview_progress);
+        previewProgress = (ProgressBar) dialogView.findViewById(R.id.preview_progress);
 
-		getDialog().setCanceledOnTouchOutside(true);
+        getDialog().setCanceledOnTouchOutside(true);
 
-		return result;
-	}
+        return dialogView;
+    }
 
 
-	protected void setContent(String content){
-		String niceContent = AwfulMessage.getMessageHtml(content, AwfulPreferences.getInstance());
-		postPreView.loadDataWithBaseURL(Constants.BASE_URL + "/", niceContent, "text/html", "utf-8", null);
-		previewProgress.setVisibility(View.GONE);
-		postPreView.setVisibility(View.VISIBLE);
-		postPreView.invalidate();
-	}
+    protected void setContent(String content) {
+        String niceContent = AwfulMessage.getMessageHtml(content, AwfulPreferences.getInstance());
+        preparePreferences();
+        postPreView.loadDataWithBaseURL(Constants.BASE_URL + "/", niceContent, "text/html", "utf-8", null);
+        previewProgress.setVisibility(View.GONE);
+        postPreView.setVisibility(View.VISIBLE);
+    }
 
-	@Override
-	public String getTitle() {
-		return "Post Preview";
-	}
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // I don't know why I have to insist on that but alright then.
+        postPreView.destroy();
+    }
 
-	@Override
-	public boolean volumeScroll(KeyEvent event) {
-		return false;
-	}
+    public void configureWebView() {
+        postPreView = (WebView) dialogView.findViewById(R.id.post_pre_view);
+        postPreView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        postPreView.getSettings().setJavaScriptEnabled(true);
+        postPreView.getSettings().setRenderPriority(WebSettings.RenderPriority.LOW);
+        postPreView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        postPreView.getSettings().setDefaultFontSize(mPrefs.postFontSizeDip);
+        postPreView.getSettings().setDefaultFixedFontSize(mPrefs.postFixedFontSizeDip);
+        if (Constants.DEBUG && AwfulUtils.isKitKat()) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+        if (AwfulUtils.isLollipop()) {
+            postPreView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        if (mPrefs.inlineYoutube || mPrefs.inlineWebm || mPrefs.inlineVines) {//YOUTUBE SUPPORT BLOWS
+            postPreView.getSettings().setPluginState(WebSettings.PluginState.ON_DEMAND);
+        }
+        if (AwfulUtils.isAtLeast(Build.VERSION_CODES.JELLY_BEAN_MR1) && (mPrefs.inlineWebm || mPrefs.inlineVines)) {
+            postPreView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        }
+        if (mPrefs.inlineTweets && AwfulUtils.isJellybean()) {
+            postPreView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+            postPreView.getSettings().setAllowFileAccessFromFileURLs(true);
+            postPreView.getSettings().setAllowFileAccess(true);
+            postPreView.getSettings().setAllowContentAccess(true);
+        }
+
+        if (!mPrefs.enableHardwareAcceleration) {
+            postPreView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        }
+        postPreView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                postPreView.loadUrl("javascript:pageinit()");
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
+        postPreView.addJavascriptInterface(this, "listener");
+    }
+
+    @JavascriptInterface
+    public String getPreference(String preference) {
+        return preferences.get(preference);
+    }
+
+    private void preparePreferences() {
+        AwfulPreferences aPrefs = AwfulPreferences.getInstance();
+
+        preferences = new HashMap<String, String>();
+        preferences.clear();
+        preferences.put("username", aPrefs.username);
+        preferences.put("youtubeHighlight", "#ff00ff");
+        preferences.put("showSpoilers", Boolean.toString(aPrefs.showAllSpoilers));
+        preferences.put("postFontSize", Integer.toString(aPrefs.postFontSizePx));
+        preferences.put("postcolor", ColorProvider.convertToARGB(ColorProvider.getTextColor()));
+        preferences.put("backgroundcolor", ColorProvider.convertToARGB(ColorProvider.getBackgroundColor()));
+        preferences.put("linkQuoteColor", ColorProvider.convertToARGB(aPrefs.getResources().getColor(R.color.link_quote)));
+        preferences.put("highlightUserQuote", Boolean.toString(aPrefs.highlightUserQuote));
+        preferences.put("highlightUsername", Boolean.toString(aPrefs.highlightUsername));
+        preferences.put("inlineTweets", Boolean.toString(aPrefs.inlineTweets));
+        preferences.put("inlineWebm", Boolean.toString(aPrefs.inlineWebm));
+        preferences.put("autostartWebm", Boolean.toString(aPrefs.autostartWebm));
+        preferences.put("inlineVines", Boolean.toString(aPrefs.inlineVines));
+        preferences.put("disableGifs", Boolean.toString(aPrefs.disableGifs));
+        preferences.put("hideSignatures", Boolean.toString(aPrefs.hideSignatures));
+        preferences.put("disablePullNext", Boolean.toString(aPrefs.disablePullNext));
+    }
+
+    @Override
+    public String getTitle() {
+        return "Post Preview";
+    }
+
+    @Override
+    public boolean volumeScroll(KeyEvent event) {
+        return false;
+    }
 }
