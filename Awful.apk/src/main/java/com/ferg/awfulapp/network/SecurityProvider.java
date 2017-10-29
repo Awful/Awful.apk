7c7685f6ac9718ca270c908ad6b74956a0da467d
baka kaba <bakakaba@email.nope>
2017-10-29 03:03:49 +0000

    Fix SSL connection failures on pre-Lollipop, also updates network security
    This uses the security provider from Google Play Services to basically patch
    the networking calls and stop old devices from trying to connect with SSLv3,
    which doesn't work anymore. It should also make things more secure in general?
    
    I'm using an old version of the auth library so I can test it on an old emulator.
    Might be worth updating later? You need Google Play Services installed in the SDK
    too
    
    I've crowbarred an SSL error check into the base request class, seems like the
    easiest place to catch it. It just pops up a toast telling people to update
    Google Play Services so the security provider can hopefully update. Feels bad
    but it's a fix.
    
    I'm working on a system for popping a useful info dialog but I'm not sure how to
    tell when it's important ("you need to update because SSL is broke as heck")
    or not ("you need to update for better security even though everything works").
    And people *really don't like popups*

---
+++ Awful.apk/src/main/java/com/ferg/awfulapp/network/SecurityProvider.java
@@ -0,0 +1,54 @@
+package com.ferg.awfulapp.network;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
+
+/**
+ * Created by baka kaba on 29/10/2017.
+ * <p>
+ * A component to handle updating the device's Security Provider, which updates to fix vulnerabilities
+ * and routes security API calls through itself.
+ * <p>
+ * The update might fail because the user needs to install or update Google Play Services manually,
+ * or it could hit an unrecoverable error - which means the app might not work at all, especially
+ * if it's an old device that's still trying to use SSLv3. The last known state is held in {@link #status}
+ * <p>
+ * See: <a href="https://developer.android.com/training/articles/security-gms-provider.html">Updating Your Security Provider to Protect Against SSL Exploits</a>
+ */
+abstract class SecurityProvider {
+
+    private static String TAG = SecurityProvider.class.getSimpleName();
+    private static Status status = Status.UNCHECKED;
+    private static boolean userNotified = false;
+
+
+    /**
+     * Update the system's security provider to fix network vulnerabilities.
+     * <p>
+     * This fixes older devices trying to connect through SSLv3 (which gets rejected).
+     */
+    static void update(@NonNull Context context) {
+        try {
+            // this here blocks if there's an update, allegedly for ~350ms on older devices
+            // calling this first when initialising NetworkUtils (and letting it block) means we can update it before the network stuff is set up
+            ProviderInstaller.installIfNeeded(context);
+            status = Status.UP_TO_DATE;
+            Log.i(TAG, "Security Provider is up to date.");
+        } catch (GooglePlayServicesRepairableException e) {
+            status = Status.PLAY_SERVICES_UPDATE_REQUIRED;
+            Log.w(TAG, "Security Provider requires a Google Play Services update.", e);
+
+        } catch (GooglePlayServicesNotAvailableException e) {
+            status = Status.PLAY_SERVICES_ERROR;
+            Log.w(TAG, "Security Provider can't update!", e);
+        }
+
+    }
+
+    enum Status {UNCHECKED, UP_TO_DATE, PLAY_SERVICES_UPDATE_REQUIRED, PLAY_SERVICES_ERROR}
+}
