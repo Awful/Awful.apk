c5286763c84827704823f32e6e75595d270c9384
joseph <josephs3d@gmail.com>
2018-01-02 12:13:24 -0600

    Remove header

--- Awful.apk/src/main/java/com/ferg/awfulapp/CrashlyticsReportingTree.kt
+++ Awful.apk/src/main/java/com/ferg/awfulapp/CrashlyticsReportingTree.kt
@@ -3,10 +3,6 @@ package com.ferg.awfulapp
 import com.crashlytics.android.Crashlytics
 import timber.log.Timber
 
-/**
- * Created by joseph on 1/2/2018.
- */
-
 
 class CrashlyticsReportingTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
