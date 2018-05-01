8b8d11dfe0f268aac2a91f2633970360f05c80d2
baka kaba <bakakaba@email.nope>
2018-05-01 02:07:04 +0100

    Clean up NavigationEvent a little
    I've just rolled some of the type ID references into the class
    constructors, so the Intent builder can pull it directly from the type
    instead of having to match and add on each one

--- Awful.apk/src/main/java/com/ferg/awfulapp/NavigationEvent.kt
+++ Awful.apk/src/main/java/com/ferg/awfulapp/NavigationEvent.kt
@@ -16,27 +16,27 @@ import timber.log.Timber
  * app can handle fully formed NavigationEvent objects instead.
  */
 
-sealed class NavigationEvent {
+sealed class NavigationEvent(private val extraTypeId: String) {
 
-    object ReAuthenticate : NavigationEvent()
-    object MainActivity : NavigationEvent()
-    object Bookmarks : NavigationEvent()
-    object ForumIndex : NavigationEvent()
-    object Settings : NavigationEvent()
-    object SearchForums : NavigationEvent()
-    object Announcements : NavigationEvent()
+    object ReAuthenticate : NavigationEvent(TYPE_RE_AUTHENTICATE)
+    object MainActivity : NavigationEvent(TYPE_MAIN_ACTIVITY)
+    object Bookmarks : NavigationEvent(TYPE_BOOKMARKS)
+    object ForumIndex : NavigationEvent(TYPE_FORUM_INDEX)
+    object Settings : NavigationEvent(TYPE_SETTINGS)
+    object SearchForums : NavigationEvent(TYPE_SEARCH_FORUMS)
+    object Announcements : NavigationEvent(TYPE_ANNOUNCEMENTS)
 
     data class Thread(val id: Int, val page: Int? = null, val postJump: String? = null) :
-            NavigationEvent()
+            NavigationEvent(TYPE_THREAD)
 
-    data class Forum(val id: Int, val page: Int? = null) : NavigationEvent()
-    data class Url(val url: AwfulURL) : NavigationEvent()
+    data class Forum(val id: Int, val page: Int? = null) : NavigationEvent(TYPE_FORUM)
+    data class Url(val url: AwfulURL) : NavigationEvent(TYPE_URL)
 
     // TODO: the activity just wants to parse an int anyway (a long is probably better), parse/validate here?
     /**
      * Show the user's private messages, with an optional URL for a message to open
      */
-    data class PrivateMessages(val messageUri: Uri? = null) : NavigationEvent()
+    data class PrivateMessages(val messageUri: Uri? = null) : NavigationEvent(TYPE_PRIVATE_MESSAGES)
 
 
     /**
@@ -44,44 +44,31 @@ sealed class NavigationEvent {
      *
      * This function produces the correctly structured Intents recognised by the app, including the
      * expected launch modes and flags. If you need to use Intents to e.g. navigate from one Activity
-     * to another, you should use this.
+     * to another, you should use this instead of constructing one in another class.
      */
-    fun getIntent(context: Context): Intent = activityIntent(context).apply {
+    fun getIntent(context: Context): Intent = activityIntent(context).putExtra(EVENT_EXTRA_KEY, extraTypeId).apply {
+        // we've created the base intent and added its type data - any other data is added here
         when (this@NavigationEvent) {
             is Thread -> {
-                putExtra(EVENT, TYPE_THREAD)
                 putExtra(Constants.THREAD_ID, id)
                 page?.let { putExtra(Constants.THREAD_PAGE, page) }
                 postJump?.let { putExtra(Constants.THREAD_FRAGMENT, postJump) }
             }
             is Forum -> {
-                putExtra(EVENT, TYPE_FORUM)
                 putExtra(Constants.FORUM_ID, id)
                 page?.let { putExtra(Constants.FORUM_PAGE, page) }
             }
-            is PrivateMessages -> {
-                putExtra(EVENT, TYPE_PRIVATE_MESSAGES)
-                messageUri?.let(::setData)
-            }
-            Bookmarks -> {
-                putExtra(EVENT, TYPE_BOOKMARKS)
-                putExtra(Constants.FORUM_ID, Constants.USERCP_ID)
-            }
-            ForumIndex -> {
-                putExtra(EVENT, TYPE_FORUM_INDEX)
-            }
-            ReAuthenticate -> {
-                putExtra(EVENT, TYPE_RE_AUTHENTICATE)
-                putExtra(TYPE_RE_AUTHENTICATE, true)
-            }
-            MainActivity -> putExtra(EVENT, TYPE_MAIN_ACTIVITY)
-            Settings -> putExtra(EVENT, TYPE_SETTINGS)
-            SearchForums -> putExtra(EVENT, TYPE_SEARCH_FORUMS)
-            Announcements -> putExtra(EVENT, TYPE_ANNOUNCEMENTS)
+            is PrivateMessages -> messageUri?.let(::setData)
         }
     }
 
 
+    /**
+     * Get the base intent for this navigation event, with the activity it opens and any special flags or configuration.
+     *
+     * Every event needs an entry here! This defines its destination, the activity that should open
+     * and handle any other data it contains.
+     */
     private fun activityIntent(context: Context): Intent = when (this) {
         MainActivity, Bookmarks, ForumIndex, is Thread, is Forum, is Url ->
             Intent().setClass(context, ForumsIndexActivity::class.java)
@@ -100,9 +87,12 @@ sealed class NavigationEvent {
 
     companion object {
 
-        private const val EVENT = "navigation event"
+        // the identifiers for each navigation type, added to and read from the app's intents, and the key used to store them
+
+        private const val EVENT_EXTRA_KEY = "navigation event"
         private const val TYPE_RE_AUTHENTICATE = "nav_re-auth"
         private const val TYPE_MAIN_ACTIVITY = "nav_main_activity"
+        /** This one gets baked into the bookmarks widget when it's created, so if you change the value they'll have to be remade */
         private const val TYPE_BOOKMARKS = "nav_bookmarks"
         private const val TYPE_FORUM_INDEX = "nav_forum_index"
         private const val TYPE_THREAD = "nav_thread"
@@ -119,7 +109,7 @@ sealed class NavigationEvent {
          */
         fun Intent.parse(): NavigationEvent {
             parseUrl()?.let { return it }
-            return when (getStringExtra(EVENT)) {
+            return when (getStringExtra(EVENT_EXTRA_KEY)) {
             //TODO: handle behaviour for missing data, e.g. can't navigate to a thread with no thread ID
                 TYPE_RE_AUTHENTICATE -> ReAuthenticate
                 TYPE_MAIN_ACTIVITY -> MainActivity
