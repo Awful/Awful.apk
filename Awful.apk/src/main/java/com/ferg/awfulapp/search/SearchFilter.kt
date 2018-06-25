378bfff4a2b7a21737ea6fa32df6b938b7d394da
baka-kaba <baka-kaba@users.noreply.github.com>
2018-06-25 01:20:01 +0100

    Rework search filters, fix NavigationEvent.Url serialisation (#624)
    The Url event wasn't fully implemented, so the #toIntent handling
    basically created an Intent with no data. I've added that, so now
    the Intents we create will parse back correctly.
    
    I've overhauled search's filtering system - now there's a separate
    SearchFilter class contains one of the filter types we handle, and
    the data for that filter. The menu is automatically populated from
    these types, each type can spawn a dialog for the data it needs, and
    the fragment gets a callback when a new filter is added.
    
    There are two ideas behind this. The first is implemented - the Search
    NavigationEvent now takes an optional set of configured SearchFilters,
    which means that other components can navigate to the Search section
    and specify some filters to populate it with. I've added a "search
    this forum" option to the thread list context menu which does this, but
    you can specify any combination. "Search for this user in this thread",
    "who quoted me today", that kind of thing.
    
    The second idea is to make the filter UI easier to work with - ideally
    something like a list of added filters, where you can edit each one
    (maybe to change some terms) or swipe it away. Right now it still adds
    each new filter to the text field like before, but they're represented
    by SearchFilter objects - the fragment just needs to store them as a
    data set and let the RecyclerView work with it. You can query each of
    them to build up the final query string for the request, and keep that
    text representation out of sight

---
+++ Awful.apk/src/main/java/com/ferg/awfulapp/search/SearchFilter.kt
@@ -0,0 +1,86 @@
+package com.ferg.awfulapp.search
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.EditText
+import com.ferg.awfulapp.R
+
+/**
+ * Created by baka kaba on 13/06/2018.
+ *
+ * Represents a filter for use in forums searches.
+ *
+ * This holds a type (reflecting the different search keywords available on the site) and the actual
+ * data being searched for / filtered on - text, IDs etc.
+ */
+class SearchFilter(val type: FilterType, val param: String) : Parcelable {
+
+    /**
+     * Describes the various types of filters available in the forums search.
+     *
+     * [filterTemplate] is used to produce the output required by the query string.
+     * [label] is a short name for the filter, and [description] is an optional longer version used
+     * in the hint text for input dialogs.
+     */
+    enum class FilterType(val filterTemplate: String, val label: String, val description: String? = null) {
+
+        PostText("%s", "Text in posts"),
+        UserId("userid:%s", "User ID"),
+        Username("username:\"%s\"", "Username"),
+        Quoting("quoting:\"%s\"", "User being quoted"),
+        Before("before:\"%s\"", "Earlier than"),
+        After("since:\"%s\"", "Later than"),
+        ThreadId("threadid:%s", "Thread ID"),
+        InTitle("intitle:\"%s\"", "Thread title", "Text in thread title");
+
+
+        /**
+         * Show a popup dialog allowing the user to add data to filter on.
+         * Sets the result on the provided #SearchFragment.
+         */
+        fun showDialog(searchFragment: SearchFragment) {
+            searchFragment.activity?.run {
+                val layout = layoutInflater.inflate(R.layout.insert_text_dialog, null)
+                val textField = layout.findViewById<View>(R.id.text_field) as EditText
+                textField.hint = description ?: label
+                AlertDialog.Builder(this)
+                        .setTitle("Add search filter")
+                        .setView(layout)
+                        .setPositiveButton("Add filter", { _, _ ->
+                            searchFragment.addFilter(SearchFilter(this@FilterType, textField.text.toString()))
+                        })
+                        .show()
+            }
+        }
+    }
+
+    override fun toString(): String = type.filterTemplate.format(param)
+
+
+    //
+    // Parcelable boilerplate - maybe use @Parcelize from Kotlin Extensions?
+    //
+
+    constructor(parcel: Parcel) : this(
+            FilterType.values()[parcel.readInt()],
+            parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(type.ordinal)
+        parcel.writeString(param)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<SearchFilter> {
+            override fun createFromParcel(parcel: Parcel): SearchFilter = SearchFilter(parcel)
+            override fun newArray(size: Int): Array<SearchFilter?> = arrayOfNulls(size)
+        }
+    }
+
+}
